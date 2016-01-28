103d829d4e5b4987052c2b5f43865f554e8cbb3e
chensonghao <719204133@qq.com>
2016-01-28 13:23:06 +0800

    add photos

--- js/main.js
+++ js/main.js
@@ -1,33 +1,45 @@
 (function() {
     //所有的数据
     var photos = [{
-        height: 950,
-        width: 800,
-        src: "imgs/1.jpg"
-    }, {
-        height: 1187,
-        width: 900,
-        src: "imgs/2.jpg"
-    }, {
-        height: 766,
-        width: 980,
-        src: "imgs/3.jpg"
-    }, {
-        height: 754,
-        width: 980,
-        src: "imgs/4.jpg"
-    }, {
-        height: 493,
-        src: "imgs/5.jpg",
-        width: 750
-    }, {
-        height: 500,
-        src: "imgs/6.jpg",
-        width: 750
+        height: 551,
+        width: 413,
+        src: "imgs/蔡文姬.jpg"
     }, {
         height: 600,
-        src: "imgs/7.jpg",
-        width: 400
+        width: 428,
+        src: "imgs/曹丕.jpg"
+    }, {
+        height: 600,
+        width: 428,
+        src: "imgs/陈琳.jpg"
+    }, {
+        height: 464,
+        width: 350,
+        src: "imgs/貂蝉.jpg"
+    }, {
+        height: 600,
+        width: 428,
+        src: "imgs/郭嘉.jpg"
+    }, {
+        height: 464,
+        width: 350,
+        src: "imgs/黄月英.jpg"
+    }, {
+        height: 464,
+        width: 350,
+        src: "imgs/小乔.jpg"
+    }, {
+        height: 550,
+        width: 413,
+        src: "imgs/张春华.jpg"
+    }, {
+        height: 551,
+        width: 413,
+        src: "imgs/甄姬.jpg"
+    }, {
+        height: 600,
+        width: 428,
+        src: "imgs/钟会.jpg"
     }];
 
     function PhotoGallery(opts) {
